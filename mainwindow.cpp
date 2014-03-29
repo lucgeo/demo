c76c50afaefea89e31512817ab7b3e37128d051a
Lucian <lucgeo92@gmail.com>
2014-03-29 11:58:32 +0200

    clean code

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,20 +1,20 @@
+#include <QFileDialog>
+#include <QGraphicsScene>
+#include <QMessageBox>
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#include "dialog.h"
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QMessageBox>
-#include "dialog.h"
-#include <QGraphicsScene>
-#include "opencv2/core/core.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include <QFileDialog>
 
-#include <iostream>
 using namespace cv;
 using namespace std;
 
-Mat img;
+Mat baseImage;
 QGraphicsScene *scene;
-
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -34,31 +34,26 @@ void MainWindow::show_image(Mat img)
     cvtColor(img, img,CV_BGR2RGB);
 
     QImage image=QImage((uchar*)img.data, img.cols, img.rows,  img.step,QImage::Format_RGB888);
+    QPixmap pixmap=QPixmap::fromImage(image);
 
-QPixmap pixmap=QPixmap::fromImage(image);
-
-scene->addPixmap(pixmap);
-ui->graphicsView->setScene(scene);
-ui->graphicsView->show();
-
+    scene->addPixmap(pixmap);
+    ui->graphicsView->setScene(scene);
+    ui->graphicsView->show();
 }
 
 
 
 void MainWindow::on_pushButton_clicked()
 {
-
     QFileDialog dialog(this);
     dialog.setNameFilter(tr("Images (*.png *.xpm *.jpg)"));
     dialog.setViewMode(QFileDialog::Detail);
- QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), "/home/lucian/Proiecte/tutorial3", tr("Images (*.png *.xpm *.jpg)"));
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), "/home/lucian/Proiecte/tutorial3", tr("Images (*.png *.xpm *.jpg)"));
 
     scene = new QGraphicsScene;
-    img = imread(fileName.toStdString(),CV_LOAD_IMAGE_COLOR);
+    baseImage = imread(fileName.toStdString(),CV_LOAD_IMAGE_COLOR);
 
-
-        show_image(img);
-
+    show_image(baseImage);
 }
 
 
@@ -70,43 +65,40 @@ void MainWindow::on_pushButton_2_clicked()
     ui->listWidget->addItem("Blur");
     ui->listWidget->addItem("toGray");
     ui->listWidget->addItem("toRGB");
-
-
 }
 
 void MainWindow::on_pushButton_3_clicked()
 {
+    Mat dest=baseImage;
+
     if(ui->listWidget_2->count()==0)
+    {
         QMessageBox::warning(this,"warning","nu ai selectat nicio functie!");
-Mat dest=img;
-   for(int i = 0; i < ui->listWidget_2->count(); i++)
-   {
+    }
 
-       QListWidgetItem* item = ui->listWidget_2->item(i);
-       if(! item->text().compare("Blur")){
-           medianBlur(dest,dest,13); }
+    for(int i = 0; i < ui->listWidget_2->count(); i++)
+    {
+        QListWidgetItem* item = ui->listWidget_2->item(i);
+        if(! item->text().compare("Blur")){
+            medianBlur(dest,dest,13); }
 
-       if(!item->text().compare("Red"))
-       {
-           inRange(dest,Scalar(0,0,0),Scalar(0,0,255),dest);
-       }
+        if(!item->text().compare("Red"))
+        {
+            inRange(dest,Scalar(0,0,0),Scalar(0,0,255),dest);
+        }
 
-       if(!item->text().compare("toGray"))
-       {
-           cvtColor(dest,dest,CV_RGB2GRAY);
-       }
+        if(!item->text().compare("toGray"))
+        {
+            cvtColor(dest,dest,CV_RGB2GRAY);
+        }
 
-       if(!item->text().compare("toRGB"))
-       {
-           cvtColor(dest,dest,CV_GRAY2RGB);
-       }
-   }
+        if(!item->text().compare("toRGB"))
+        {
+            cvtColor(dest,dest,CV_GRAY2RGB);
+        }
+    }
 
-//inRange(img,Scalar(127,127,127),Scalar(255,255,255),img);
-//QMessageBox::warning(this,"titlu","red");}
-
-show_image(dest);
-
+    show_image(dest);
 }
 
 
